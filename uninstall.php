1c2345e5d0e1d1768935a70013c1e3c07f2fc184
Batou @ MTCapthca <48454462+batou-mtcapthca@users.noreply.github.com>
2021-02-19 00:42:15 -0500

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -1,12 +1,40 @@
 <?php
-if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-	die( 'Direct access not allowed!' );
+
+if (!defined('WP_UNINSTALL_PLUGIN')) {
+    die('Direct access not allowed');
 }
 
-function mt_captcha_delete($array) {
-	foreach ($array as $one) {
-		delete_option("mt_captcha_{$one}");
-	}	
+
+/**
+ * Class MTCaptchaUninstall
+ */
+class MTCaptchaUninstall
+{
+    /**
+     * MTCaptchaUninstall constructor.
+     */
+    public function __construct()
+    {
+        $this->mt_captcha_delete([
+                    "mt_site_private_key",
+                    "mt_captcha_enable",
+                    "mt_captcha_disable_mtcaptcha",
+                    "mt_captcha_show_captcha_label_form",
+                    "mt_site_key",
+                    "mt_captcha_theme",
+                    "mt_captcha_lang",
+                    "mt_captcha_widget_size",
+                    "mt_captcha_enable_json_value",
+                    "mt_captcha_json_value"
+                ]);
+    }
+
+    private function mt_captcha_delete($array)
+    {
+        foreach ($array as $item) {
+            delete_option(sprintf('mtcaptcha_%s', $item));
+        }
+    }
 }
 
-mt_captcha_delete(array("mt_captcha_site_key", "mt_captcha_site_private_key", "mt_captcha_theme", "mt_captcha_lang", "mt_captcha_disable_mtcaptcha"));
+new MTCaptchaUninstall();
